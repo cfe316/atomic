d21a0aaa13defe7f9b2ed200ecc3da493984a419
Jacob Schwartz <thesquarerootofjacob@gmail.com>
2016-04-06 16:20:16 -0400

    Updated adas.py to reflect new OpenADAS site

--- atomic/adas.py
+++ atomic/adas.py
@@ -1,11 +1,12 @@
 import os
+import shutil
 import urllib
 
 open_adas = 'http://open.adas.ac.uk/'
 
 class OpenAdas(object):
-    def search_adf11(self, element, year='', metastable='unresolved'):
-        p = [('element', element), ('year', year), ('metastable', metastable),
+    def search_adf11(self, element, year='', ms='metastable_unresolved'):
+        p = [('element', element), ('year', year), (ms, 1),
                 ('searching', 1)]
         s = AdasSearch('adf11')
         return s.search(p)
@@ -16,33 +17,27 @@ class OpenAdas(object):
         s = AdasSearch('adf15')
         return s.search(p)
 
-    def fetch(self, id_filename, dst_directory=None):
+    def fetch(self, url_filename, dst_directory=None):
         if dst_directory == None:
             dst_directory = os.curdir
         self.dst_directory = dst_directory
 
-        dst_filename = os.path.join(self.dst_directory, id_filename[1])
+        dst_filename = os.path.join(self.dst_directory, url_filename[1])
 
-        url = self._construct_url(id_filename)
+        url = self._construct_url(url_filename)
         tmpfile, msg = urllib.urlretrieve(url)
-        lines = open(tmpfile).readlines()
 
-        # Write all lines in the destination file but the first.  Files, incorrectly,
-        # contain an empty first line. This is a bug in the Open-ADAS database.
-        dst = open(dst_filename, 'w')
-        dst.writelines(lines[1:])
+        shutil.move(tmpfile, dst_filename)
 
-        os.remove(tmpfile)
-
-    def _construct_url(self, id_filename):
+    def _construct_url(self, url_filename):
         """
-        >>> db = OpenADAS()
-        >>> db._construct_url((12345, 'foo.dat'))
-        'http://open.adas.ac.uk/download.php?id=12345'
+        >>> db = OpenAdas()
+        >>> db._construct_url(('detail/adf11/prb96/prb96_c.dat', 'foo.dat'))
+        'http://open.adas.ac.uk/download/adf11/prb96/prb96_c.dat'
         """
-        id_, filename = id_filename
-        query = [('id', id_)]
-        return open_adas + 'download.php?' + urllib.urlencode(query)
+        url, filename = url_filename
+        query = url.replace('detail','download')
+        return open_adas + query
 
 
 class AdasSearch(object):
@@ -77,14 +72,13 @@ class AdasSearch(object):
         db = []
         for l in lines:
             if self.class_ == 'adf11':
-                element, class_, comment, year, resolved, url, name = l
-                id_ = self._strip_url(url)
-                class_ = class_.lower()
-                db.append((id_, name))
+                element, class_, comment, year, resolved, url, cl, typ, name = l
+                name = name.strip()
+                db.append((url, name))
             elif self.class_ == 'adf15':
-                element, ion, w_lo, w_hi, url, name = l
-                id_ = self._strip_url(url)
-                db.append((id_, name))
+                element, ion, w_lo, w_hi, url, cl, typ, name = l
+                name = name.strip()
+                db.append((url, name))
             else:
                 raise NotImplementedError('this should never happen')
 
@@ -95,7 +89,6 @@ class AdasSearch(object):
         return int(id_)
 
 
-
 from HTMLParser import HTMLParser
 class SearchPageParser(HTMLParser):
     """
@@ -103,7 +96,7 @@ class SearchPageParser(HTMLParser):
     with a table that has the following structure:
 
     >>> html = '''
-    ... <table class='searchresults'>
+    ... <table summary='Search Results'>
     ...     <tr>
     ...     <td>Ne</td> <td><a href='filedetail.php?id=32147'>rc89_ne.dat</a></td>
     ...     <tr>
@@ -127,9 +120,20 @@ class SearchPageParser(HTMLParser):
 
         HTMLParser.reset(self)
 
+    #def handle_starttag(self, tag, attrs):
+    #    attrs = dict(attrs)
+    #    if tag == 'table' and attrs.get('class') == 'searchresults':
+    #        self.search_results = True
+    #    if not self.search_results: return
+    #
+    #    if tag == 'a' and self.line != None:
+    #        self.line.append(attrs['href'])
+
     def handle_starttag(self, tag, attrs):
         attrs = dict(attrs)
-        if tag == 'table' and attrs.get('class') == 'searchresults':
+        if (tag == 'table' 
+                and 'summary' in attrs
+                and 'Results' in attrs['summary']): 
             self.search_results = True
         if not self.search_results: return
 
