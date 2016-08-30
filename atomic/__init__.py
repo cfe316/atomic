9a61000b48063a0ccd0c649b2ef3cc0d3fa637de
Jacob Schwartz <thesquarerootofjacob@gmail.com>
2016-08-30 16:40:32 -0400

    Creation of ElectronCooling class.

--- atomic/__init__.py
+++ atomic/__init__.py
@@ -2,6 +2,7 @@ from atomic_data import AtomicData
 from coronal import CoronalEquilibrium
 from time_dependent_rates import RateEquations, RateEquationsWithDiffusion
 from radiation import Radiation
+from electron_cooling import ElectronCooling
 
 element = AtomicData.from_element
 
