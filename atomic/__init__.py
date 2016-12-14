5e5617eff02aaf4ce5448c44f0b32fff72815b69
Jacob Schwartz <thesquarerootofjacob@gmail.com>
2016-12-14 14:38:20 -0500

    Removed 'Post integral' functionality from the package.
    For a while I was working on code to make plots similar to those in
    D. Post's 1995 papers, of the parallel heat flux that can be dissipated
    by a SOL of a certain impurity content using radiation and conduction
    from a lower temperature to some higher temperature.
    
    See the paper 'Calculations of energy losses due to atomic processes in tokamaks with
    applications to the International Thermonuclear Experimental Reactor'
    
    Anyway I decided that this functionality, built on top of the fundamental
    zero-D collisional radiative (with optional diffusion lifetime tau) calculations,
    was not logically part of the package at the same level. Perhaps I will make another package
    for those calculations that uses these as input though.

--- atomic/__init__.py
+++ atomic/__init__.py
@@ -3,7 +3,6 @@ from collisional_radiative import CollRadEquilibrium
 from time_dependent_rates import RateEquations, RateEquationsWithDiffusion
 from radiation import Radiation
 from electron_cooling import ElectronCooling
-from post_integral import rhs
 
 element = AtomicData.from_element
 
