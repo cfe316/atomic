da651070e56cecbfea54f930eff857c751c2eb15
Unterberg <unterberge@fusion.gat.com>
2017-04-01 10:19:53 -0700

    small changes

--- examples/collisional_radiative_equilibrium.py
+++ examples/collisional_radiative_equilibrium.py
@@ -2,25 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import atomic
+from atomic.collisional_radiative import CollRadEquilibrium
 
-elements = ['Li', 'C', 'N', 'Ne', 'Ar']
+elements = ['Li', 'C', 'N', 'Ne', 'Si']
 
 temperature_ranges = {
-    'Li' : np.logspace(-1,3, 300),
-    'C'  : np.logspace(0,3, 300),
-    'N'  : np.logspace(0,3, 300),
-    'Ne' : np.logspace(0,4, 300),
-    'Ar' : np.logspace(0,5, 300),
+    'Li': np.logspace(-1, 3, 300),
+    'C': np.logspace(0, 3, 300),
+    'N': np.logspace(0, 3, 300),
+    'Ne': np.logspace(0, 4, 300),
+    'Si': np.logspace(0, 5, 300),
 }
 
 for element in elements:
     ad = atomic.element(element)
-    collrad = atomic.CollRadEquilibrium(ad)
+    collrad = CollRadEquilibrium(ad)
 
     temperature = temperature_ranges.get(element, np.logspace(0, 3, 300))
     y = collrad.ionisation_stage_distribution(temperature, density=1e19)
 
-    plt.figure();
+    plt.figure()
     y.plot_vs_temperature()
     #plt.savefig('collrad_equilibrium_%s.pdf' % element)
 
