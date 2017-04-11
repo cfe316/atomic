15d021bed41d52cc6e223a57340f3c83d849677c
Unterberg <unterberge@fusion.gat.com>
2017-04-11 07:26:37 -0700

    allow writing of HDF files.

--- atomic/ensemble_average.py
+++ atomic/ensemble_average.py
@@ -101,7 +101,7 @@ def time_dependent_power_z(solution, times, f_imp=1., ensemble_average=False, co
     return {'element': element, '<Z>': z_mean, 'power': power}
 
 
-def time_dependent_z_Plot(solution, times, ensemble_average=False):
+def time_dependent_z_Plot(solution, times, ensemble_average=False, plotIT=True, write2HDF=False):
     element = solution.atomic_data.element
     if ensemble_average:
         solution = solution.ensemble_average()
@@ -109,21 +109,38 @@ def time_dependent_z_Plot(solution, times, ensemble_average=False):
     else:
         title = element + r' time dependent $\left<Z\right>$'
 
-    ax = plt.gca()
-    for y in solution.select_times(times):
-        ax.loglog(solution.temperature, y.mean_charge(), color='black', ls='--')
+    if plotIT:
+        ax = plt.gca()
 
-    ax.set_xlabel(r'$T_\mathrm{e}\ \mathrm{(eV)}$')
-    ax.set_ylim(0.4, y.atomic_data.nuclear_charge + 4)
-    annotate_lines(['$10^{%d}$' % i for i in np.log10(times * solution.density)])
+    Z_mean_ntau = {}
+    for i, y in enumerate(solution.select_times(times)):
+        nam = "Z_ne_taures_"+str(times[i] * solution.density)
+        print nam
+        Z_mean_ntau[nam] = y.mean_charge()
+        if plotIT:
+            ax.loglog(solution.temperature, y.mean_charge(), color='black', ls='--')
 
-    z_mean = solution.y_coronal.mean_charge()
-    ax.loglog(solution.temperature, z_mean, color='black')
+    if plotIT:
+        ax.set_xlabel(r'$T_\mathrm{e}\ \mathrm{(eV)}$')
+        ax.set_ylim(0.4, y.atomic_data.nuclear_charge + 4)
+        annotate_lines(['$10^{%d}$' % i for i in np.log10(times * solution.density)])
 
-    ax.set_title(title)
+    z_mean_coronal = solution.y_coronal.mean_charge()
+    if plotIT:
+        ax.loglog(solution.temperature, z_mean_coronal, color='black')
+        ax.set_title(title)
+
+    if write2HDF:
+        import Misc.hickle.hickle as hkl
+        print 'START: HDF5 file save'
+        outDICT = {'temperature_arr': solution.temperature, 'coronal_Z': z_mean_coronal,
+                   'Z_ntau_DIC': Z_mean_ntau}
+        fnam = 'Zmeans_'+element+'.h5'
+        hkl.dump(outDICT, fnam)
 
 
-def time_dependent_power_Plot(solution, times, ensemble_average=False):
+def time_dependent_power_Plot(solution, times, ensemble_average=False, plotIT=True,
+                              write2HDF=False):
     element = solution.atomic_data.element
     if ensemble_average:
         solution = solution.ensemble_average()
@@ -131,30 +148,45 @@ def time_dependent_power_Plot(solution, times, ensemble_average=False):
     else:
         title = element + r' time dependent power'
 
-    ax = plt.gca()
-    for y in solution.select_times(times):
+    if plotIT:
+        ax = plt.gca()
+    Cooling_ntau = {}
+    for i, y in enumerate(solution.select_times(times)):
+        nam = "Cooling_ne_taures"+str(times[i] * solution.density)
+        print nam
         rad = atomic.Radiation(y)
-        ax.loglog(solution.temperature, rad.specific_power['total'], color='black', ls='--')
+        Cooling_ntau[nam] = rad.specific_power['total']
+        if plotIT:
+            ax.loglog(solution.temperature, rad.specific_power['total'], color='black', ls='--')
 
-    ax.set_xlabel(r'$T_\mathrm{e}\ \mathrm{(eV)}$')
-    ax.set_ylabel(r'$P/n_\mathrm{i} n_\mathrm{e}\ [\mathrm{W m^3}]$')
-    annotate_lines(['$10^{%d}$' % i for i in np.log10(times * solution.density)])
+    if plotIT:
+        ax.set_xlabel(r'$T_\mathrm{e}\ \mathrm{(eV)}$')
+        ax.set_ylabel(r'$P/n_\mathrm{i} n_\mathrm{e}\ [\mathrm{W m^3}]$')
+        annotate_lines(['$10^{%d}$' % i for i in np.log10(times * solution.density)])
 
     power_coronal = atomic.Radiation(solution.y_coronal).specific_power['total']
-    ax.loglog(solution.temperature, power_coronal, color='black')
+    if plotIT:
+        ax.loglog(solution.temperature, power_coronal, color='black')
+        ax.set_title(title)
 
-    ax.set_title(title)
+    if write2HDF:
+        import Misc.hickle.hickle as hkl
+        print 'START: HDF5 file save'
+        outDICT = {'temperature_arr': solution.temperature, 'P_coronal': power_coronal,
+                   'Cooling_ntau_DIC': Cooling_ntau}
+        fnam = 'CoolingRate_'+element+'.h5'
+        hkl.dump(outDICT, fnam)
 
 
 if __name__ == '__main__':
     times = np.logspace(-7, 0, 100)
     temperature = np.logspace(0, 4, 100)
-    density = 1e19
+    density = 1e20
 
     rt = atomic.RateEquations(atomic.element('carbon'))
     y = rt.solve(times, temperature, density)
 
-    taus = np.array([1e13, 1e14, 1e15, 1e16, 1e17, 1e18])/density
+    taus = np.array([1e14, 1e15, 1e16, 1e17, 1e18, 1e19])/density
 
     plt.figure(1)
     plt.clf()
