43662a6417a9d589bac2ab49e5b9b5441adf1115
Daan van Vugt <daanvanvugt@gmail.com>
2017-11-30 21:17:35 +0100

    Set path to find _xxdata.so files

--- atomic/__init__.py
+++ atomic/__init__.py
@@ -1,3 +1,5 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 from .atomic_data import AtomicData
 from .collisional_radiative import CollRadEquilibrium
 from .time_dependent_rates import RateEquations, RateEquationsWithDiffusion
