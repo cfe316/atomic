9182912dadd750dfbb69770870bd321d09985589
Zeke Unterberg <unterbergea@icloud.com>
2018-03-14 11:30:16 -0400

    fixed fro python3

--- atomic/adas.py
+++ atomic/adas.py
@@ -1,53 +1,74 @@
 import os
-import urllib
+import errno
+import shutil
+import urllib.request
+import urllib.parse
+import urllib.error
 
 open_adas = 'http://open.adas.ac.uk/'
 
 
 class OpenAdas(object):
-    def search_adf11(self, element, year='', metastable='unresolved'):
-        if metastable is 'unresolved':
-            p = [('element', element), ('year', year), ('metastable_unresolved', 1),
-                 ('searching', 1)]
-        else:
-            p = [('element', element), ('year', year), ('metastable_resolved', 1),
-                 ('searching', 1)]
+    def search_adf11(self, element, year='', ms='metastable_unresolved'):
+        p = [('element', element), ('year', year), (ms, 1), ('searching', 1)]
         s = AdasSearch('adf11')
         return s.search(p)
 
     def search_adf15(self, element, charge=''):
-        p = [('element', element), ('charge', charge), ('resolveby', 'file'),
-             ('searching', 1)]
+        p = [('element', element), ('charge', charge), ('resolveby', 'file'), ('searching', 1)]
         s = AdasSearch('adf15')
         return s.search(p)
 
-    def fetch(self, id_filename, dst_directory=None):
+    def fetch(self, url_filename, dst_directory=None):
         if dst_directory is None:
             dst_directory = os.curdir
         self.dst_directory = dst_directory
 
-        dst_filename = os.path.join(self.dst_directory, id_filename[1])
+        url = self._construct_url(url_filename)
+        nested = False  # this switch makes files save flat
+        if nested:
+            path = self._construct_path(url_filename)
+        else:
+            __, path = url_filename
 
-        url = self._construct_url(id_filename)
-        tmpfile, msg = urllib.urlretrieve(url)
-        lines = open(tmpfile).readlines()
+        dst_filename = os.path.join(self.dst_directory, path)
+        if not os.path.exists(dst_filename):
+            tmpfile, __ = urllib.request.urlretrieve(url)
 
-        # Write all lines in the destination file but the first.  Files, incorrectly,
-        # contain an empty first line. This is a bug in the Open-ADAS database.
-        dst = open(dst_filename, 'w')
-        dst.writelines(lines)
-        dst.close()
-        os.remove(tmpfile)
+            self._mkdir_p(os.path.dirname(dst_filename))
 
-    def _construct_url(self, id_filename):
+            shutil.move(tmpfile, dst_filename)
+
+    def _construct_url(self, url_filename):
         """
-        >>> db = OpenADAS()
-        >>> db._construct_url((directory, 'foo.dat'))
-        'http://open.adas.ac.uk/download.php?id=12345'
+        >>> db = OpenAdas()
+        >>> db._construct_url(('detail/adf11/prb96/prb96_c.dat', 'foo.dat'))
+        'http://open.adas.ac.uk/download/adf11/prb96/prb96_c.dat'
         """
-        id_, filename = id_filename
-        query = [('id', id_)]
-        return open_adas + 'download/' + id_[7:]
+        url, __ = url_filename
+        query = url.replace('detail', 'download')
+        return open_adas + query
+
+    def _construct_path(self, url_filename):
+        """
+        This function constructs a path to store the file in.
+        >>> db = OpenAdas()
+        >>> db._construct_path(('detail/adf11/prb96/prb96_c.dat', 'foo.dat'))
+        'adf11/prb96/prb96_c.dat'
+        """
+        url, filename = url_filename
+        path = url.replace('detail/', '')
+        path = path.replace('][', '#')
+        return path
+
+    def _mkdir_p(self, path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:  # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
 
 
 class AdasSearch(object):
@@ -66,8 +87,8 @@ class AdasSearch(object):
         return self._parse_data()
 
     def _retrieve_search_page(self):
-        search_url = self.url + urllib.urlencode(self.parameters)
-        res, msg = urllib.urlretrieve(search_url)
+        search_url =  self.url + urllib.parse.urlencode(self.parameters)
+        res, __ = urllib.request.urlretrieve(search_url)
         self.data = open(res).read()
         os.remove(res)
 
@@ -76,48 +97,36 @@ class AdasSearch(object):
         parser.feed(self.data)
         lines = parser.lines
 
-        if lines == []:
-            return {}
+        if lines == []: return {}
         header = lines.pop(0)
 
         db = []
         for l in lines:
             if self.class_ == 'adf11':
-                element = l[0]
-                class_ = l[6]+l[7]
-                comment = l[2]
-                year = l[3]
-                resolved = l[4]
-                # url = l[6]
-                name = l[8].lstrip()
-                # id_ = self._strip_url(url)
-                id_ = l[5]
-                class_ = class_.lower()
-                db.append((id_, name))
+                element, class_, comment, year, resolved, url, cl, typ, name = l
+                name = name.strip()
+                db.append((url, name))
             elif self.class_ == 'adf15':
-                element, ion, w_lo, w_hi, url, name = l
-                id_ = self._strip_url(url)
-                db.append((id_, name))
+                element, ion, w_lo, w_hi, url, cl, typ, name = l
+                name = name.strip()
+                db.append((url, name))
             else:
                 raise NotImplementedError('this should never happen')
 
         return db
 
     def _strip_url(self, url):
-        _, id_ = url.split('=')
+        __, id_ = url.split('=')
         return int(id_)
 
 
-from HTMLParser import HTMLParser
-
-
+from html.parser import HTMLParser
 class SearchPageParser(HTMLParser):
     """
     Filling in a search form on http://open.adas.ac.uk generates a HTML document
     with a table that has the following structure:
-
     >>> html = '''
-    ... <table class='searchresults'>
+    ... <table summary='Search Results'>
     ...     <tr>
     ...     <td>Ne</td> <td><a href='filedetail.php?id=32147'>rc89_ne.dat</a></td>
     ...     <tr>
@@ -125,7 +134,6 @@ class SearchPageParser(HTMLParser):
     ...     <td>C</td> <td><a href='filedetail.php?id=32154'>rc89_c.dat</a></td>
     ...     </tr>
     ... </table>'''
-
     The SearchPageParser can parse this document looking for a table with a
     class `searchresults`.
     >>> parser = SearchPageParser()
@@ -141,21 +149,30 @@ class SearchPageParser(HTMLParser):
 
         HTMLParser.reset(self)
 
+    #def handle_starttag(self, tag, attrs):
+    #    attrs = dict(attrs)
+    #    if tag == 'table' and attrs.get('class') == 'searchresults':
+    #        self.search_results = True
+    #    if not self.search_results: return
+    #
+    #    if tag == 'a' and self.line != None:
+    #        self.line.append(attrs['href'])
+
     def handle_starttag(self, tag, attrs):
         attrs = dict(attrs)
-        if tag == 'table' and str(attrs.get('summary')).find('Results') != -1:
+        if (tag == 'table'
+                and 'summary' in attrs
+                and 'Results' in attrs['summary']):
             self.search_results = True
-        if not self.search_results:
-            return
+        if not self.search_results: return
 
-        if tag == 'a' and self.line is not None:
+        if tag == 'a' and self.line != None:
             self.line.append(attrs['href'])
 
     def handle_endtag(self, tag):
         if tag == 'table':
             self.search_results = False
-        if not self.search_results:
-            return
+        if not self.search_results: return
 
         if tag == 'tr':
             self.lines.append(self.line)
