07ee9872e75cf93fc83e03fc0d0c46be1dbb21b0
Zeke Unterberg <unterbergea@icloud.com>
2018-03-14 11:30:53 -0400

    Fixed for package import

--- atomic/__init__.py
+++ atomic/__init__.py
@@ -1,8 +1,10 @@
-from atomic_data import AtomicData
-from coronal import CoronalEquilibrium
-from time_dependent_rates import RateEquations
-from radiation import Radiation
-
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+from .atomic_data import AtomicData
+from .collisional_radiative import CollRadEquilibrium
+from .time_dependent_rates import RateEquations, RateEquationsWithDiffusion
+from .radiation import Radiation
+from .electron_cooling import ElectronCooling
 
 element = AtomicData.from_element
-
